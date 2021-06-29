24a7ff8ad48b47536a1fcdb5d62ee7014be54d45
Simanga Hendry Khoza <h3ndry@users.noreply.github.com>
2021-06-29 22:26:16 +0000

    test: data, customer route

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import config from 'config';
 import log from './logger';
 import express from "express";
 import {createConnection} from "typeorm";
-import { SimCard } from "./entity/SimCard";
+import { Customer } from './entity/Customer';
+// import { SimCard } from "./entity/SimCard";
 import router from "./routes"
 // const router = require('./routes') ;
 
@@ -30,18 +31,63 @@ const host = config.get("host") as string;
 
     })
 
-
-    // log.info("Inserting a new user into the database...");
-    // const simCard = new SimCard();
-    // simCard.name = "Timber";
+    // simCard.name = "Unique Name 3";
     // await connection.manager.save(simCard);
-    // log.info("Saved a new user with id: " + simCard.id);
+    // log.info("Saved a new user with id: " + simCard.simid);
 
     console.log("Loading users from the database...");
-    const simCards = await connection.manager.find(SimCard);
+    const simCards = await connection.manager.find(Customer);
     // const simCards = await connection.manager.remove(i)
     console.log("Loaded users: ", simCards);
 
     console.log("Here you can setup and run express/koa/any other framework.");
 
 }).catch(error => console.log(error));
+
+
+
+
+//     const customer = new Customer();
+//     customer.firstName = "customer one first name"
+//     customer.lastName = "customer one lastname"
+//     customer.age = 27
+//     customer.email = "test@t.ca"
+
+
+//     const customer_1 = new Customer();
+//     customer_1.firstName = "customer two first name"
+//     customer_1.lastName = "customer two lastname"
+//     customer_1.age = 30
+//     customer_1.email = "test_1@t.ca"
+
+
+//     const customer_2 = new Customer();
+//     customer_2.firstName = "customer three first name"
+//     customer_2.lastName = "customer three lastname"
+//     customer_2.age = 17
+//     customer_2.email = "test_2@t.ca"
+
+
+//     const customer_3 = new Customer();
+//     customer_3.firstName = "customer four first name"
+//     customer_3.lastName = "customer four lastname"
+//     customer_3.age = 27
+//     customer_3.email = "test_4@t.ca"
+
+
+//     const customer_4 = new Customer();
+//     customer_4.firstName = "customer five first name"
+//     customer_4.lastName = "customer five lastname"
+//     customer_4.age = 27
+//     customer_4.email = "test_5@t.ca"
+
+
+//     await connection.manager.save(customer);
+//     await connection.manager.save(customer_1);
+//     await connection.manager.save(customer_2);
+//     await connection.manager.save(customer_3);
+//     await connection.manager.save(customer_4);
+
+
+
+
--- src/routes.ts
+++ src/routes.ts
@@ -1,22 +1,17 @@
 import express from 'express';
 import { getAllSimCard } from './controller/SimCardController'
+import { getAllCustomers } from './controller/CustomerControler';
 import { Request, Response } from "express";
 // import {Repository } from 'typeorm'
-
 
 const router = express.Router();
 
 
-//  One endpoint to list available SIM cards
-// Each SIM card must have a unique ID and Name
 router.get("/healthcheck", (_:Request, res:Response) => res.send("oka"));
 
+//  One endpoint to list available SIM cards
+// Each SIM card must have a unique ID and Name
 router.get("/sim-cards", getAllSimCard)
-
-
-export default router;
-
-//    app.get("/healthcheck", (_, res: Response) => res.sendStatus(200));
 
 
 //    // o One endpoint to create an order (see sample)
@@ -24,9 +19,20 @@ export default router;
 //    // 1. SIM card id (Taken from list of SIM cards that have unique IDs)
 //    // 2. Customer Id (Customer must have a name and unique ID)
 //    // 3. Delivery address (of customer),
+router.get("/orders")
+router.post("/orders")
+router.put("/orders/:id")
 
-//    app.get("/orders")
-//    app.put("/orders/:id")
+
+
+// not asked path
+router.get("/customers", getAllCustomers);
+
+export default router;
+
+//    app.get("/healthcheck", (_, res: Response) => res.sendStatus(200));
+
+
 
 //    //more
 //    app.delete("/")
