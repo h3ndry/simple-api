1f90013302066bc27ee276bda62d1532c4ca686f
Simanga Hendry Khoza <h3ndry@users.noreply.github.com>
2021-07-10 09:05:33 +0000

    clean address

---
+++ src/entity/Address.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
+
+@Entity()
+export class Address extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    city: string;
+
+    @Column()
+    province: string;
+
+    @Column()
+    country: string;
+
+    @Column()
+    zipCode: number;
+
+    @Column()
+    agent: string;
+}
