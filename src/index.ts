07d481f58f9d96f5afc6dd935a5054a73e2002b0
Simanga Hendry Khoza <h3ndry@users.noreply.github.com>
2021-07-10 09:31:53 +0000

    clean

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,26 @@
 import "reflect-metadata";
-import config from 'config';
-import log from './logger';
+import config from "config";
+import log from "./logger";
 import express from "express";
-import {createConnection} from "typeorm";
-// import { Customer } from './entity/Customer';
-// import { SimCard } from "./entity/SimCard";
-import router from "./routes"
+import { createConnection } from "typeorm";
+import router from "./routes";
 
+createConnection()
+    .then(async () => {
+        const port = config.get("port") as number;
+        const host = config.get("host") as string;
 
-createConnection().then(async () => {
+        const app = express();
 
-    const port = config.get("port") as number;
-    const host = config.get("host") as string;
+        app.use(express.json());
+        app.use(express.urlencoded({ extended: false }));
 
-    const app = express();
-
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }))
-
-    // 1. Must be a RESTful HTTP API listening to port 3200 (or you can use another port instead
-    app.listen(port, host, () => {
-
-        log.info(`Server Listen at http://${host}:${port}`);
-
-        app.use('/', router)
+        // 1. Must be a RESTful HTTP API listening to port 3200 (or you can use another port instead
+        app.listen(port, host, () => {
+            log.info(`Server Listen at http://${host}:${port}`);
+            app.use("/", router);
+        });
     })
-
-}).catch(error => console.log(error));
-
+    .catch((error) => log.error(error));
 
 // ============================================================
